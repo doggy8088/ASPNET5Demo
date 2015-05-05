ec051c558d07a49866895ceb01bafd04e7c177b3
Will <doggy.huang@gmail.com>
2015-05-06 02:28:58 +0800

    建立三個 ASP.NET 5 專案範本
    A01_Empty: VS2015RC 預設的 Empty 專案範本
    A02_WebAPI: VS2015RC 預設的 Web API 專案範本
    A03_WebSite: VS2015RC 預設的 Web Site 專案範本

---
+++ A03_WebSite/src/A03_WebSite/Properties/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A03_WebSite
+{
+    public class AppSettings
+    {
+        public string SiteTitle { get; set; }
+    }
+}
